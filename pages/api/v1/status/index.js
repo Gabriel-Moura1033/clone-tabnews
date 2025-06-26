dd8ec4faa90892d5d66ba0ccf9c29ab1bbdf3d27
Gabriel Moura <128106537+Gabriel-Moura1033@users.noreply.github.com>
2025-06-26 00:42:25 +0000

    adicionando jsconfig.json

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,7 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as NUMBER;");
+  const result = await database.query("SELECT 11 + 1 as NUMBER;");
   console.log(result.rows);
   response.status(200).json({ chave: "teste" });
 }
